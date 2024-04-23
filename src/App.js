728b63ac4020bf0777c28302d8c75d5f9a473bde
Homero Ramirez <homeroram@outlook.com>
2024-04-23 15:12:49 -0500

    basic setup adding and removing files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/vending machine.jpeg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What would you like to eat today?
         </p>
         <a
           className="App-link"
